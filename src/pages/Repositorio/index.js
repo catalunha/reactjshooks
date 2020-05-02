9fee6443d10fd8dbc337985cd848e307a69fa8f2
catalunha <catalunha.mj@gmail.com>
2020-05-02 20:36:18 -0300

    aula98

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Owner, Loading, BackButton, IssuesList } from './styles'
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './styles'
 import { FaArrowLeft } from 'react-icons/fa'
 import api from '../../services/api'
 
@@ -8,6 +8,7 @@ function Respositorio({ match }) {
   const [repositorio, setRepositorio] = useState({})
   const [issues, setIssues] = useState([])
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
     async function load() {
@@ -30,7 +31,23 @@ function Respositorio({ match }) {
     }
     load()
   }, [match.params.repo])
-
+  useEffect(()=>{
+    async function loadIssue() {
+      const nomeRepo = decodeURIComponent(match.params.repo)
+      const response = await api.get(`/repos/${nomeRepo}/issues`,{
+        params:{
+          state:'open',
+          page:page,
+          per_page:5,
+        }
+      })
+      setIssues(response.data)
+    }
+    loadIssue()
+  },[match.params.repo,page])
+  function handlePage(action) {
+    setPage(action === 'back' ? page - 1 : page + 1)
+  }
   if (loading) {
     return (
       <Loading>
@@ -62,11 +79,15 @@ function Respositorio({ match }) {
                   <span key={String(label.id)}>{label.name}</span>
                 ))}
               </strong>
-                <p>{issue.user.login}</p>
+              <p>{issue.user.login}</p>
             </div>
           </li>
         ))}
       </IssuesList>
+      <PageActions>
+        <button type='button' onClick={() => handlePage('back')} disabled={page<2}>Voltar</button>
+        <button type='button' onClick={() => handlePage('next')}>Proxima</button>
+      </PageActions>
     </Container>
   )
 }

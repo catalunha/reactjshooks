05bceccfa16f35b7c058888571cd33cb1995eac1
catalunha <catalunha.mj@gmail.com>
2020-05-02 10:40:14 -0300

    aula82

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ]);
+  const [input, setInput] = useState('')
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
   return (
     <div>
-      Hooks
+      <ul>
+        {tarefas.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+      <button type='button' onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }

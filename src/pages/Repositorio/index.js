8f29778cdc42c56e578cbe5d7444db12c618b140
catalunha <catalunha.mj@gmail.com>
2020-05-02 19:40:19 -0300

    aula96

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react'
-import { Container } from './styles'
+import { Container, Owner, Loading, BackButton } from './styles'
+import { FaArrowLeft } from 'react-icons/fa'
 import api from '../../services/api'
 
 // decodeURIComponent(match.params.repo)
 function Respositorio({ match }) {
-  const [repositorio,setRepositorio] = useState({})
-  const [issues,setIssues] = useState([])
-  const [loaing,setLoading] = useState(true)
+  const [repositorio, setRepositorio] = useState({})
+  const [issues, setIssues] = useState([])
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     async function load() {
       const nomeRepo = decodeURIComponent(match.params.repo)
       console.log(nomeRepo)
-      const [repoData,repoIssues] = await Promise.all([
+      const [repoData, repoIssues] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
-        api.get(`/repos/${nomeRepo}/issues`,{
+        api.get(`/repos/${nomeRepo}/issues`, {
           params: {
-            state:'open',
-            per_page:5
+            state: 'open',
+            per_page: 5
           }
         })
       ])
@@ -29,9 +31,26 @@ function Respositorio({ match }) {
     load()
   }, [match.params.repo])
 
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    )
+  }
   return (
     <Container>
+      <BackButton to='/'>
+        <FaArrowLeft color='#000' size={35} />
 
+      </BackButton>
+      <Owner>
+        <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
+      </Owner>
+      <h1>
+        {repositorio.name}
+      </h1>
+      <p>{repositorio.description}</p>
     </Container>
   )
 }

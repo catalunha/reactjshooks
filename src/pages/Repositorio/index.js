e79a64117b1b0178d0e8ee42c87ebfd29a1fd3a6
catalunha <catalunha.mj@gmail.com>
2020-05-02 17:37:50 -0300

    aula95

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container } from './styles'
+import api from '../../services/api'
 
-function Respositorio({match}) {
+// decodeURIComponent(match.params.repo)
+function Respositorio({ match }) {
+  const [repositorio,setRepositorio] = useState({})
+  const [issues,setIssues] = useState([])
+  const [loaing,setLoading] = useState(true)
+  useEffect(() => {
+    async function load() {
+      const nomeRepo = decodeURIComponent(match.params.repo)
+      console.log(nomeRepo)
+      const [repoData,repoIssues] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`,{
+          params: {
+            state:'open',
+            per_page:5
+          }
+        })
+      ])
+      console.log(repoData.data)
+      console.log(repoIssues.data)
+      setRepositorio(repoData.data)
+      setIssues(repoIssues.data)
+      setLoading(false)
+    }
+    load()
+  }, [match.params.repo])
+
   return (
-    <h1 style={{ color: '#FFF' }}>
-      Repositorio: 
-      {decodeURIComponent(match.params.repo)}
-      </h1>
+    <Container>
+
+    </Container>
   )
 }
 
